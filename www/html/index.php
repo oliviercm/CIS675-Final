db87bd8aad6f044159c7bc74e015386405777980
Olivier Chan <olivierchanmoy@gmail.com>
2021-11-22 21:42:25 -0800

    Add sidebar nav

--- www/html/index.php
+++ www/html/index.php
@@ -9,16 +9,39 @@
     <link rel="stylesheet" href="./styles/global.css">
     <link rel="stylesheet" href="./styles/google-material-icons.css">
     <link rel="stylesheet" href="./styles/header.css">
+    <link rel="stylesheet" href="./styles/main.css">
     <link rel="stylesheet" href="./styles/footer.css">
 </head>
 
 <body>
     <?php include $_SERVER['DOCUMENT_ROOT']."/html/header.php";?>
-    <main>
-        <div>
-            Hello World!
+    <div class="grid-container">
+        <div class="sidebar">
+            <nav class="sidebar-nav">
+                <span style="font-size: 1.2em;">Administration</span>
+                <hr>
+                <ul>
+                    <li>
+                        <a href="./index.php">All Locations</a>
+                    </li>
+                    <li>
+                        <a href="./index.php">All Products</a>
+                    </li>
+                    <li>
+                        <b>All Employees</b>
+                    </li>
+                    <li>
+                        <a href="./index.php">All Customers</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
-    </main>
+        <main>
+            <div>
+                Tabled data goes here
+            </div>
+        </main>
+    </div>
     <?php include $_SERVER['DOCUMENT_ROOT']."/html/footer.php";?>
 </body>
 
