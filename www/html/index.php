e9589a1619467a64cd69b0d9d5ecf9d652b6614b
Olivier Chan <olivierchanmoy@gmail.com>
2021-11-22 22:47:48 -0800

    Display static table data

--- www/html/index.php
+++ www/html/index.php
@@ -37,8 +37,49 @@
             </nav>
         </div>
         <main>
-            <div>
-                Tabled data goes here
+            <div class="main-table-container">
+                <table>
+                    <tr>
+                        <th>Employee ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Address</th>
+                        <th>Phone</th>
+                        <th>Salary</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Malcom</td>
+                        <td>McDuff</td>
+                        <td>10 Castle Drive, London</td>
+                        <td>4085552241</td>
+                        <td>55780.71</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Fleance</td>
+                        <td>Banquo</td>
+                        <td>77 Rampart Way, London</td>
+                        <td>4085558810</td>
+                        <td>70213.88</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Lord</td>
+                        <td>Macbeth</td>
+                        <td>1 Scotland Yard, London</td>
+                        <td>4085556312</td>
+                        <td>80123.56</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Lady</td>
+                        <td>Macbeth</td>
+                        <td>1 Scotland Yard, London</td>
+                        <td>4085557723</td>
+                        <td>10782.44</td>
+                    </tr>
+                </table>
             </div>
         </main>
     </div>
