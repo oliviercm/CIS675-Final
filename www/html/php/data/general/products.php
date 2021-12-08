7ca52037e79e88630ad2ff8515a599ffc72587f9
Olivier Chan <olivierchanmoy@gmail.com>
2021-12-07 23:25:28 -0800

    Update products.php

--- www/html/php/data/general/products.php
+++ www/html/php/data/general/products.php
@@ -3,12 +3,30 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/php/util/mysql.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/php/util/respondjson.php";
 
 if ($_SERVER['REQUEST_METHOD'] === "GET") {
-    try {
-        $db = MySQL::getInstance();
-        $result = $db->executeQuery("SELECT * FROM Product");
-        respondJson($result);
-    } catch (\Throwable $e) {
-        http_response_code(500);
+    if (empty($_GET["id"])) { // No ID specified - fetch all products
+        try {
+            $db = MySQL::getInstance();
+            $result = $db->executeQuery("SELECT * FROM Product;");
+            respondJson($result);
+        } catch (\Throwable $e) {
+            http_response_code(500);
+        }
+    } else if (!is_numeric($_GET["id"])) {
+        http_response_code(400);
+        echo("Error: Invalid product ID.");
+    } else { // Specific product ID specified - fetch specific product
+        try {
+            $db = MySQL::getInstance();
+            $result = $db->executeQuery("SELECT * FROM Product WHERE id = ?;", $_GET["id"]);
+            if ($result === "false") {
+                http_response_code(404);
+                echo("Error: Product not found.");
+            } else {
+                respondJson($result[0]);
+            };
+        } catch (\Throwable $e) {
+            http_response_code(500);
+        }
     }
 } else {
     http_response_code(405);
